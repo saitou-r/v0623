b6ed7ad5d59cb739ab26be540e59a91d5c361a0b
A061-09 <tnk.dat.class@gmail.com>
2022-06-23 13:24:55 +0900

    fuku

--- v0623/Form1.cs
+++ v0623/Form1.cs
@@ -12,10 +12,41 @@ namespace v0623
 {
     public partial class Form1 : Form
     {
+        int vx = -10;
+        int vy = -10;
+
         public Form1()
         {
             InitializeComponent();
         }
 
+        private void label1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("齋藤麗也");
+            timer1.Enabled = false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Left += vx;
+            label1.Top += vy;
+
+            if (label1.Left < 0)
+            {
+                vx = 10;
+            }
+            else if (label1.Top < 0)
+            {
+                vy = 10;
+            }
+            else if (label1.Left > ClientSize.Width)
+            {
+                vx = -10;
+            }
+            else if (label1.Top > ClientSize.Height)
+            {
+                vy = -10;
+            }
+        }
     }
 }
